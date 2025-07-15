8f1510bc04567031acb23c9d8bd90a7e197fa0d6
nathy-cpu <degeadavid042@gmail.com>
2025-07-16 02:16:28 +0300

    Add lines to separate the blocks

--- src/blocks.rs
+++ src/blocks.rs
@@ -1,5 +1,6 @@
 use raylib::{
     color::Color,
+    math::{Rectangle, Vector2},
     prelude::{RaylibDraw, RaylibDrawHandle},
 };
 
@@ -33,15 +34,36 @@ impl Blocks {
     pub fn draw(&self, draw_handle: &mut RaylibDrawHandle) {
         for y in 0..NUM_BLOCKS_Y {
             for x in 0..NUM_BLOCKS_X {
+                // dont ask why it just works
                 if self.blocks[y][x] {
-                    // dont ask why it just works
-                    draw_handle.draw_rectangle(
-                        20 + x as i32 * BLOCK_WIDTH as i32,
-                        40 + y as i32 * BLOCK_HEIGHT as i32,
-                        BLOCK_WIDTH as i32,
-                        BLOCK_HEIGHT as i32,
-                        self.row_colors[y],
-                    );
+                    let block_rect = Rectangle {
+                        x: 20.0 + x as f32 * BLOCK_WIDTH,
+                        y: 40.0 + y as f32 * BLOCK_HEIGHT,
+                        width: BLOCK_WIDTH,
+                        height: BLOCK_HEIGHT,
+                    };
+                    let top_left = Vector2 {
+                        x: block_rect.x,
+                        y: block_rect.y,
+                    };
+                    let top_right = Vector2 {
+                        x: block_rect.x + BLOCK_WIDTH,
+                        y: block_rect.y,
+                    };
+                    let bottum_left = Vector2 {
+                        x: block_rect.x,
+                        y: block_rect.y + BLOCK_HEIGHT,
+                    };
+                    let bottum_right = Vector2 {
+                        x: block_rect.x + BLOCK_WIDTH,
+                        y: block_rect.y + BLOCK_HEIGHT,
+                    };
+                    let thickness = 1.0;
+                    draw_handle.draw_rectangle_rec(block_rect, self.row_colors[y]);
+                    draw_handle.draw_line_ex(top_left, top_right, thickness, Color::LIGHTGRAY);
+                    draw_handle.draw_line_ex(top_left, bottum_left, thickness, Color::LIGHTGRAY);
+                    draw_handle.draw_line_ex(top_right, bottum_right, thickness, Color::DARKGRAY);
+                    draw_handle.draw_line_ex(bottum_left, bottum_right, thickness, Color::DARKGRAY);
                 }
             }
         }
