64b177ae46a68341db7b8f15958bd56dc9ae12ae
nathy-cpu <degeadavid042@gmail.com>
2025-07-15 21:07:48 +0300

    Add reflection for bouncing the ball

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,9 @@
-use raylib::{RaylibHandle, ffi::KeyboardKey, math::Vector2, prelude::RaylibDrawHandle};
+use raylib::{
+    RaylibHandle,
+    ffi::KeyboardKey,
+    math::{Rectangle, Vector2},
+    prelude::RaylibDrawHandle,
+};
 
 use crate::{
     SCREEN_SIZE,
@@ -32,6 +37,8 @@ impl GameState {
     }
 
     pub fn update(&mut self, raylib_handle: &RaylibHandle) {
+        let prev_ball_pos = self.ball.position;
+
         if !self.started {
             self.ball.position.y = BALL_START_POS_Y;
             self.ball.position.x = (SCREEN_SIZE as f32 / 2.0)
@@ -54,10 +61,49 @@ impl GameState {
             .position
             .x
             .clamp(0.0, (SCREEN_SIZE - PADDLE_HEIGHT) as f32);
+
+        // reflect ball off of the paddle
+        let paddle_rect = Rectangle {
+            x: self.paddle.position.x,
+            y: self.paddle.position.y,
+            width: PADDLE_WIDTH as f32,
+            height: PADDLE_HEIGHT as f32,
+        };
+        if paddle_rect.check_collision_circle_rec(self.ball.position, self.ball.radius) {
+            let mut collision_normal: Vector2 = Default::default();
+
+            if prev_ball_pos.y < paddle_rect.y + paddle_rect.height {
+                collision_normal += Vector2 { x: 0.0, y: -1.0 };
+                self.ball.position.y = paddle_rect.y - self.ball.radius;
+            }
+            if prev_ball_pos.y > paddle_rect.y + paddle_rect.height {
+                collision_normal += Vector2 { x: 0.0, y: 1.0 };
+                self.ball.position.y = paddle_rect.y + paddle_rect.height + self.ball.radius;
+            }
+            if prev_ball_pos.x < paddle_rect.x {
+                collision_normal += Vector2 { x: -1.0, y: 0.0 };
+            }
+            if prev_ball_pos.x > paddle_rect.x + paddle_rect.width {
+                collision_normal += Vector2 { x: 1.0, y: 0.0 };
+            }
+
+            if collision_normal != Vector2::zero() {
+                reflect(&mut self.ball.direction, collision_normal);
+            }
+        }
     }
 
     pub fn draw(&self, draw_handle: &mut RaylibDrawHandle) {
         self.ball.draw(draw_handle);
         self.paddle.draw(draw_handle);
     }
+}
+
+// ig i gotta do it myself
+fn reflect(vector: &mut Vector2, normal: Vector2) {
+    let dot_product = vector.dot(normal);
+    *vector = Vector2 {
+        x: vector.x - (2.0 * normal.x) * dot_product,
+        y: vector.y - (2.0 * normal.y) * dot_product,
+    };
 }
